7eb02056feef2ae0b6be91dfd046a5f4c185c522
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-12-12 18:02:48 -0800

    server created

--- src/server.py
+++ src/server.py
@@ -1,2 +1,23 @@
-"""This module contains the server side of the CF 401 - Python HTTP-server assignment."""
+"""Server for Echo server assignment."""
 
+
+import socket
+
+
+def server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    address = ('127.0.0.1', 5000)
+    server.bind(address)
+
+    server.listen(1)
+
+    conn, addr = server.accept()
+
+    buffer_length = 10
+    echo = ''
+    while True:
+        echo += conn.recv(buffer_length).decode('utf8')
+        if len(conn.recv(buffer_length)) < buffer_length:
+            break
+
+    conn.sendall(echo.encode('utf8'))
