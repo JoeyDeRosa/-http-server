8dcd747a92f19a95a05b5f02e321e6d7f60aa761
Ted Callahan <CCallahanIV@gmail.com>
2016-12-28 10:22:20 -0800

    Updated README and tests.

--- src/test_servers.py
+++ src/test_servers.py
@@ -2,34 +2,6 @@
 
 import pytest
 
-# Test the following:
-# messages shorter than one buffer in length
-# messages longer than several buffers in length
-# messages that are an exact multiple of one buffer in length
-# messages containing non-ascii characters
-
-# def test_client_short():
-#     """Test with message shorter than one buffer length."""
-#     from client import client
-#     test_msg = "twerp"
-#     return client(test_msg) == u"twerp"
-
-# def test_client_long():
-#     """Test with message longer than several buffers in length."""
-#     from client import client
-#     test_msg = "This is a very long test message."
-#     return client(test_msg) == test_msg
-
-# def test_client_exact_buffer():
-#     """Test messages that are an exact multiple of one buffer in length."""
-#     from client import client
-#     test_msg = "abcdefghij"
-#     return client(test_msg) == test_msg
-
-
-# def test_client_non_ASCII():
-#     """Test messages that contain non ASCII characters."""
-#     from client import client
 
 """BEGIN TESTS FOR STEP1 ASSIGNMENT"""
 
@@ -37,22 +9,22 @@ import pytest
 # Host: www.example.com<CRLF>
 # <CRLF>
 
-GOOD_REQ = u"GET /index.html HTTP/1.1\\r\\nHost: www.example.com\\r\\n\\r\\n"
+GOOD_REQ = u"GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n"
 BAD_REQs = [
-    b"GOT /index.html HTTP/1.1\\r\\nHost: www.example.com\\r\\n\\r\\n",
-    b"GET /index.html HTTP/0.1\\r\\nHost: www.example.com\\r\\n\\r\\n",
-    b"GET index.html HTTP/0.1\\r\\nHost: www.example.com\\r\\n\\r\\n",
+    u"GOT /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n",
+    u"GET /index.html HTTP/0.1\r\nHost: www.example.com\r\n\r\n",
+    u"GET index.html HTTP/0.1\r\nHost: www.example.com\r\n\r\n",
 ]
 
 
-def test_test_request_good_req():
+def test_parse_request_good_req():
     """Test test_request() with a properly formatted HTTP message."""
     from server import parse_request
-    assert parse_request(GOOD_REQ.encode('utf-8')) == b"/index.html"
+    assert parse_request(GOOD_REQ) == "/index.html"
 
 
 @pytest.mark.parametrize("req", BAD_REQs)
-def test_test_request_bad_req(req):
+def test_parse_request_bad_req(req):
     """Test test_requrest() witih an improperly formatted HTTP message."""
     from server import parse_request
     assert parse_request(req) == "400"
@@ -61,11 +33,11 @@ def test_test_request_bad_req(req):
 def test_response_ok():
     """Test ok response."""
     from client import client
-    assert b'200' == client(GOOD_REQ)[9:12]
+    assert '200' == client(GOOD_REQ)[9:12]
 
 
 @pytest.mark.parametrize("req", BAD_REQs)
 def test_response_err(req):
     """Test response err."""
     from client import client
-    assert b'500' == client(req)[9:12]
+    assert '400' == client(req)[9:12]
